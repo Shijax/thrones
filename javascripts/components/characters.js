708bede5a0355903ee431a1df25ca42f6fca7eb7
Matthew Jackson <mattshiloh@gmail.com>
2018-10-01 21:22:47 -0500

    images

--- javascripts/components/characters.js
+++ javascripts/components/characters.js
@@ -1,22 +1,29 @@
 import { printToDom } from "../helpers/util.js";
 
 const characters = [
-    {id: 'character1', name: 'Eddard Stark', house: 'Stark', imgUrl: 'yaddayadda'},
-    {id: 'character2', name: 'Catelyn Stark', house: 'Stark', imgUrl: 'yaddayadda'},
-    {id: 'character3', name: 'Sansa Stark', house: 'Stark', imgUrl: 'yaddayadda'},
-    {id: 'character4', name: 'Arya Stark', house: 'Stark', imgUrl: 'yaddayadda'}
+    { id: 'character1', name: 'Eddard Stark', house: 'Stark', imgUrl: '../images/ned.jpg' },
+    { id: 'character2', name: 'Catelyn Stark', house: 'Stark', imgUrl: '../images/catelyn.jpg' },
+    { id: 'character3', name: 'Sansa Stark', house: 'Stark', imgUrl: '../images/sansa.jpg' },
+    { id: 'character4', name: 'Arya Stark', house: 'Stark', imgUrl: '../images/arya.jpg' }
 ];
 
 const characterBuilder = () => {
     let domString = '';
     characters.forEach((character) => {
-        domString += `<h1>${character.name}<h1>`;
+        domString += `<div class="col-2 character">`
+        domString += `<div class="card">`;
+        domString += `<img class="card-img-top" src="${character.imgUrl}" alt="${character.name}">`;
+        domString += `<div class="card-body">`;
+        domString += `<h5 class="card-title">${character.name}</h5>`;
+        domString += `</div>`;
+        domString += `</div>`;
+        domString += `</div>`;
     });
     printToDom(domString);
 };
 
 const attachEvents = () => {
-   characterBuilder();
+    characterBuilder();
 }
 
-export {attachEvents}
+export { attachEvents }
